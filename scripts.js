bf5eab77c2224669b33d8007bf72ba5389082692
Evan McLean <evanhughmclean@gmail.com>
2022-04-23 15:40:17 -0400

    DOM manipulation to display computer and user attacks

--- scripts.js
+++ scripts.js
@@ -9,6 +9,19 @@ const computerScore = document.querySelector("#computerScore");
     return gameActions[Math.floor(Math.random() * gameActions.length)];
 }
 
+//function for returning an image for what weapon the computer chose
+function computerImage(action) {
+    if (action === "Rock") {
+        return "./images/rock.png";
+    }
+    else if (action === "Paper") {
+        return "./images/paper.png";
+    }
+    else {
+        return "./images/scissors.png";
+    }
+}
+
 //Function that plays round of rock paper scissors, updates player scores, returns
 //win, draw, or lose message
 function playRound(playerSelection, computerSelection) {
@@ -58,20 +71,35 @@ function resetGame() {
 
 //Create function to play game of rock paper scissors
 function game() {
+    const arena = document.querySelector('.arena');
+    const computerWeapon = document.createElement('img');
+    const playerWeapon = document.createElement('img');
     const btn = document.querySelectorAll('.weapon');
+    const vs = document.querySelector(".arena p");
+    const computer = document.querySelector("#computer");
     //adds event listeners to the buttons to play a round once clicked
     btn.forEach ((button) => button.addEventListener('click', function() {
         let playerSelection;
         let computerSelection = computerPlay();
+        let computerAttack = computerImage(computerSelection);
+        vs.remove();
+        arena.insertBefore(playerWeapon,computer);
+        arena.insertBefore(computerWeapon, computer);
+        computerWeapon.classList.add("weapon");
+        playerWeapon.classList.add("weapon");
+        computerWeapon.src = computerAttack;
         if (button.classList.contains('rock')) {
+            playerWeapon.src = "./images/rock.png";
             playerSelection = "Rock";
             playRound(playerSelection, computerSelection);
         }
         else if (button.classList.contains('paper')) {
+            playerWeapon.src = "./images/paper.png";
             playerSelection = "Paper";
             playRound(playerSelection, computerSelection);
         }
         else {
+            playerWeapon.src = "./images/scissors.png";
             playerSelection = "Scissors";
             playRound(playerSelection, computerSelection);
         }
